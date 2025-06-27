d25694fa552dd9ffafbaad37e2cf8027a6fdb601
Vrushabh Kakadiya <155346369+vrushabh3639@users.noreply.github.com>
2025-06-27 04:14:09 +0000

    pure ts

--- 02basics/myInterface.ts
+++ 02basics/myInterface.ts
@@ -1,1 +1,30 @@
-interface
+interface User {
+    readonly dbId: number
+    email: string
+    userId: number
+    googleId?: string
+    // startTrial: () => string
+    startTrial(): string
+    getCoupon(couponname: string, value: number): number
+}
+
+interface User {
+    githubToken: string
+}
+
+interface Admin extends User {
+    role: "admin" | "ta" | "learner"
+}
+
+const vrushabh: Admin = {dbId: 1234, email: "v@v.com", userId: 123,
+    role: "admin",
+    githubToken: "github",
+    startTrial: () => {
+        return "trial started"
+    },
+    getCoupon: (name: "vrushabh10", off: 10) => {
+        return 10
+    }
+}
+vrushabh.email = "v@vk.com"
+// vrushabh.dbId = 2322
