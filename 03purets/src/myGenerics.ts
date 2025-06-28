f0dfe5d8417dcf87c78732be7413d9eb7b66205b
Vrushabh <vrushabhkakadiya3639@gmail.com>
2025-06-28 17:15:46 +0530

    finally generics is over

--- 03purets/src/myGenerics.ts
+++ 03purets/src/myGenerics.ts
@@ -52,3 +52,21 @@ function anotherFunction<T, U extends Database>(valOne: T, valTwo: U): object {
 }
 
 // anotherFunction(3, {})
+
+interface Quiz {
+    name: string,
+    type: string
+}
+
+interface Course {
+    name: string,
+    author: string,
+    subject: string
+}
+
+class Sellable<T> {
+    public cart: T[] = []
+    addToCart(product: T) {
+        this.cart.push(product)
+    }
+}
