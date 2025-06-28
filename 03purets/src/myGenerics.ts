c0b99280328b974ba7b8b22fbb6fbacccac7728a
Vrushabh <vrushabhkakadiya3639@gmail.com>
2025-06-28 16:33:25 +0530

    generics

---
+++ 03purets/src/myGenerics.ts
@@ -0,0 +1,14 @@
+const score: Array<number> = []
+const names: Array<string> = []
+
+function identityOne(val: boolean | number): boolean | number {
+    return val
+}
+
+function identityTwo(val: any): any {
+    return val
+}
+
+function identityThree<Type> (val: Type): Type {
+    return val
+}
