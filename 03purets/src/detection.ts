f21b2baf02b8294408c5d8019458603d2bea170c
Vrushabh <vrushabhkakadiya3639@gmail.com>
2025-06-29 09:47:21 +0530

    in and instanceof

--- 03purets/src/detection.ts
+++ 03purets/src/detection.ts
@@ -24,3 +24,28 @@ function printALl(strs: string | string[] | null) {
         }
     }
 }
+
+interface User {
+    name: string,
+    email: string
+}
+
+interface Admin {
+    name: string,
+    email: string,
+    isAdmin: boolean
+}
+
+function isAdminAccount(account: User | Admin) {
+    if ("isAdmin" in account) {
+        return account.isAdmin
+    }
+}
+
+function logValue(x: Date | string) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    } else {
+        console.log(x.toUpperCase());
+    }
+}
