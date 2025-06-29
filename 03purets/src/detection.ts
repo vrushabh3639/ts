b58c7c75f8118b031b65a0fb7e137fa1fbe99fed
Vrushabh <vrushabhkakadiya3639@gmail.com>
2025-06-29 09:54:36 +0530

    more about narrowing

--- 03purets/src/detection.ts
+++ 03purets/src/detection.ts
@@ -49,3 +49,20 @@ function logValue(x: Date | string) {
         console.log(x.toUpperCase());
     }
 }
+
+type Fish = {swim: () => void}
+type Bird = {fly: () => void}
+
+function isFish(pet: Fish | Bird): pet is Fish {
+    return (pet as Fish).swim !== undefined
+}
+
+function getFood(pet: Fish | Bird) {
+    if (isFish(pet)) {
+        pet
+        return "fish food"
+    } else {
+        pet
+        return "bird food"
+    }
+}
